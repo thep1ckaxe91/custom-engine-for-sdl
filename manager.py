16af4731517919070bd0e8397c8cca09d0864317
thep1ckaxe91 <98675539+thep1ckaxe91@users.noreply.github.com>
2024-03-05 11:06:11 +0700

    polishing and finish, decompressed the engine.hpp to multiple file for mainability

--- manager.py
+++
@@ -1,7 +0,0 @@
-with open("data.txt","r") as f:
-    data = f.readlines()
-    for line in data:
-        name, code_val = line.split("#")
-        val = code_val.split()[1].strip()
-        val = val.removeprefix("(").removesuffix(")").split(",")
-        print("{\""+name.strip("\t")+"\",{"+val[0]+","+val[1]+","+val[2]+"}},")
